c5cbc1baddf45e8ff7fd0bcac8b8219cfa0407fa
nimninja <nimayshah1118@gmail.com>
2026-09-11 12:57:08 -0700

    added Planet class

--- src/Planet.java
+++ src/Planet.java
@@ -1,4 +1,12 @@
 public class Planet {
+    double myXPos; // current x positio
+    double myYPos; // current y positio
+    double myXVel; // current velocity
+    double myYVel; // current velocity
+    double myMass; // mass of planet
+    String myFileName;
+
+
     public static void main(String[] args) {
 
     }
